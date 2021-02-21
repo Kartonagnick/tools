194695bcb5ae7e53eb1eb8b8ebc1695781b10d7a
Kartonagnick <CastleOfDreams@yandex.ru>
2021-02-21 23:56:03 +0300

    add: platform/windows

---
+++ include/tools/windows.hpp
@@ -0,0 +1,24 @@
+
+// [2021y-02m-21d] Idrisov Denis R.
+
+#pragma once
+#ifndef dTOOLS_WINDOWS_USED_ 
+#define dTOOLS_WINDOWS_USED_ 100
+//==============================================================================
+//==============================================================================
+
+#ifndef _WIN32_WINNT
+    #define _WIN32_WINNT 0x0502
+#endif
+
+#ifdef _WIN32
+    #define NOMINMAX 1
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h> 
+#else
+     #error: only windows supported
+#endif
+
+//==============================================================================
+//==============================================================================
+#endif // !dTOOLS_WINDOWS_USED_
