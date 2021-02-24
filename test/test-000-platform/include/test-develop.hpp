8840558dea8c8d5a0b6b32db14565bf3ff78d8af
Kartonagnick <CastleOfDreams@yandex.ru>
2021-02-25 01:08:07 +0300

    add: test-000-platform

---
+++ test/test-000-platform/include/test-develop.hpp
@@ -0,0 +1,23 @@
+
+// [2021y-02m-16d] Idrisov Denis R.
+#pragma once
+#ifndef dTEST_DEVELOP_USED_ 
+#define dTEST_DEVELOP_USED_ 1
+
+    #define CODE_GENERATION_ON
+    #define INCLUDE_AUTO_GENERATED
+
+    // #define INCLUDE_STRESS_TESTS
+    // #define INCLUDE_LONG_LONG_TESTS
+    // #define INCLUDE_LONG_TESTS
+
+//==============================================================================
+//===== tools/features =====================================||==================
+    #define TEST_TOOLS_FEATURE_ATOMIC                       // ready
+    #define TEST_TOOLS_FEATURE_RVALUE_REFERENCE             // ready
+    #define TEST_TOOLS_FEATURE_NULLPTR                      // ready
+//==============================================================================
+//==============================================================================
+// in progress...
+
+#endif // !dTEST_DEVELOP_USED_
