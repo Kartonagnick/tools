ecabee3d1a4d5bf98f12a769c06f8f232d063a77
Kartonagnick <CastleOfDreams@yandex.ru>
2021-02-27 19:54:47 +0300

    add: test-flag-synch

--- test/test-600-others/sources/tools/flag/test-flag-synch-cpp98.cpp
+++ test/test-600-others/sources/tools/flag/test-flag-synch-cpp98.cpp
@@ -5,12 +5,11 @@
 
 #ifdef TEST_TOOLS_FLAG
 
-#include <tools/flag.hpp>
-
 #define dTEST_COMPONENT tools
 #define dTEST_METHOD flag
-#define dTEST_TAG synch
+#define dTEST_TAG synch_cpp98
 
+#include <tools/flag.hpp>
 namespace me = ::tools;
 //==============================================================================
 //==============================================================================
@@ -24,13 +23,16 @@ namespace
         eTWO   = 1<<2,
         eTHREE = 1<<3
     };
-    typedef me::flag<eFLAGS> 
-        flag_t;
+	typedef me::flag<eFLAGS> flag_t;
 
-} // namespace
+}//namespace
 
 //==============================================================================
 //==============================================================================
+
+TEST_COMPONENT(000) 
+{
+}
 
 //==============================================================================
 //==============================================================================
