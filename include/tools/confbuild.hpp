4500d642d1d91c89ceb186675378cc0a11606225
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-08 03:32:53 +0300

    version 0.1.4

--- include/tools/confbuild.hpp
+++ include/tools/confbuild.hpp
@@ -10,7 +10,7 @@
 // [2021y-03m-05d][23:19:34] Idrisov Denis R. 011
 // [2021y-03m-06d][04:06:55] Idrisov Denis R. 012
 // [2021y-03m-07d][10:34:03] Idrisov Denis R. 013
-// [2021y-03m-08d][03:25:15] Idrisov Denis R. 014
+// [2021y-03m-08d][03:31:39] Idrisov Denis R. 014
 #pragma once
 #ifndef dTOOLS_CONFBUILD_USED_
 #define dTOOLS_CONFBUILD_USED_ 014
