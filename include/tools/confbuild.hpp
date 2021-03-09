7477b01e0fda8e0004ec4705faa6ba740c322010
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-09 21:05:00 +0300

    version 0.1.7

--- include/tools/confbuild.hpp
+++ include/tools/confbuild.hpp
@@ -13,10 +13,10 @@
 // [2021y-03m-08d][03:31:39] Idrisov Denis R. 014
 // [2021y-03m-08d][18:44:19] Idrisov Denis R. 015
 // [2021y-03m-09d][01:44:12] Idrisov Denis R. 016
-// [2021y-03m-09d][20:55:31] Idrisov Denis R. 017 PRE
+// [2021y-03m-09d][21:02:02] Idrisov Denis R. 017
 #pragma once
 #ifndef dTOOLS_CONFBUILD_USED_
-#define dTOOLS_CONFBUILD_USED_ 017 PRE
+#define dTOOLS_CONFBUILD_USED_ 017
 //==============================================================================
 //==============================================================================
 
