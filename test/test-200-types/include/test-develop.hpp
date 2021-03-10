277f884a1a1b6b525e09982d5df2e8b73717778a
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-10 23:25:14 +0300

    add: test-200-types

---
+++ test/test-200-types/include/test-develop.hpp
@@ -0,0 +1,21 @@
+// [2021y-03m-10d][12:46:41] Idrisov Denis R.
+#pragma once
+#ifndef dTEST_DEVELOP_USED_ 
+#define dTEST_DEVELOP_USED_ 1
+
+    #define CODE_GENERATION_ON
+    #define INCLUDE_AUTO_GENERATED
+
+    // #define INCLUDE_STRESS_TESTS
+    #define INCLUDE_LONG_LONG_TESTS
+    #define INCLUDE_LONG_TESTS
+
+//==============================================================================
+//===== modern/types =======================================||==================
+    #define TEST_FIXED                                      // ready!
+
+//==============================================================================
+//==============================================================================
+// in progress...
+
+#endif // !dTEST_DEVELOP_USED_
