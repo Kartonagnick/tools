39927a36fd7a9cde6a3b2c4902747019e9b0677f
Kartonagnick <CastleOfDreams@yandex.ru>
2021-03-11 10:00:44 +0300

    done: constexpr

--- include/tools/cast.hpp
+++ include/tools/cast.hpp
@@ -49,6 +49,18 @@ namespace tools
             typedef typename x::type type;
         };
 
+        template<class r, class t>
+        dCONSTEXPR_CPP11 r max_value() dNOEXCEPT
+        {
+            return static_cast<r>(tools::limit<t>::max_value);
+        }
+
+        template<class r,class t>
+        dCONSTEXPR_CPP11 r min_value() dNOEXCEPT
+        {
+            return static_cast<r>(tools::limit<t>::min_value);
+        }
+
 
         template<class ret_type, class from_type>
         class help
@@ -162,7 +174,8 @@ namespace tools
             template<class ret, class from>
             static dCONSTEXPR_CPP11 bool can(const from v) dNOEXCEPT
             {
-                return static_cast<from>(limit<ret>::max_value) >= v;
+                return max_value<from,ret>() >= v;
+                //return static_cast<from>(limit<ret>::max_value) >= v;
             }
         };
 
@@ -174,7 +187,8 @@ namespace tools
             {
                 typedef select_big<ret, from> x;
                 typedef typename x::type big;
-                return static_cast<big>(limit<ret>::max_value) >= static_cast<big>(v);
+                return max_value<big,ret>() >= static_cast<big>(v);
+                //return static_cast<big>(limit<ret>::max_value) >= static_cast<big>(v);
             }
         };
 
@@ -188,6 +202,7 @@ namespace tools
                 typedef typename x::type big;
 
                 return v < 0 ? false:
+                    //max_value<big,ret>() >= static_cast<big>(v);
                     static_cast<big>(limit<ret>::max_value) >= static_cast<big>(v);
             }
         };
@@ -202,8 +217,10 @@ namespace tools
                 typedef typename x::type big;
 
                 return
-                    static_cast<big>(limit<ret>::max_value) >= static_cast<big>(v) &&
-                    static_cast<big>(limit<ret>::min_value) < static_cast<big>(v);
+                    max_value<big,ret>() >= static_cast<big>(v) &&
+                    min_value<big,ret>() <  static_cast<big>(v) ;
+                    //static_cast<big>(limit<ret>::max_value) >= static_cast<big>(v) &&
+                    //static_cast<big>(limit<ret>::min_value) < static_cast<big>(v);
             }
         };
 
@@ -247,275 +264,7 @@ namespace tools
     }
 
 } // namespace tools
+
+//================================================================================
+//================================================================================
 #endif // !dTOOLS_NUMERIC_CAST_USED_
-
-//================================================================================
-//================================================================================
-//================================================================================
-//================================================================================
-//================================================================================
-//================================================================================
-
-
-#if 0
-// [2020y-09-25d][20:13:18] Idrisov Denis R.
-// [2020y-12-01d][14:06:17] Idrisov Denis R.
-// [2021y-02-20d][18:40:18] Idrisov Denis R.
-// [2021y-03-10d][05:33:36] Idrisov Denis R.
-#pragma once
-#ifndef dTOOLS_NUMERIC_CAST_USED_ 
-#define dTOOLS_NUMERIC_CAST_USED_ 1
-
-#include <tools/features.hpp>
-#include <tools/types/fixed.hpp>
-//================================================================================
-//================================================================================
-//================================================================================
-//================================================================================
-namespace tools
-{
-    namespace detail_cast
-    {
-        template<class ret, class from, bool sigret>
-        struct positiv
-        {
-            static dCONSTEXPR_CPP11 bool can(const from v) dNOEXCEPT 
-            {
-                return v >= 0;
-            }
-        };
-
-        template<class ret, class from>
-        struct positiv<ret, from, true>
-        {
-            static dCONSTEXPR_CPP11 bool can(const from v) dNOEXCEPT 
-            {
-                typedef limit<ret> lim;
-                return static_cast<ret>(lim::min_value) <= static_cast<ret>(v);
-            }
-        };
-
-//.................................................................................
-        template<class ret, class from, bool capacious>
-            struct diff;
-
-        template<class ret, class from, bool capacious>
-        struct diff
-        {
-            // small -> big
-            static dCONSTEXPR_CPP11 bool can(const from v) dNOEXCEPT 
-            {
-                typedef is_signed<ret> sig;
-                //typedef positiv<ret, from, sig::value> p;
-                //return p::can(v);
-                typedef limit<ret> lim;
-                return v >= 0 || (sig::value && static_cast<ret>(lim::min_value) <= static_cast<ret>(v));
-            }
-        };
-
-        template<class ret, class from> struct diff<ret, from, false>
-        {
-            static dCONSTEXPR_CPP11 bool can(const from v) dNOEXCEPT 
-            { 
-                typedef limit<ret> lim;
-                return static_cast<from>(lim::max_value) >= v &&
-                    static_cast<from>(lim::min_value) <= v;
-            }
-        };
-//.................................................................................
-        template<class from> struct diff<float, from, false>
-        {
-            static dCONSTEXPR_CPP11 
-            bool can(const from) dNOEXCEPT { return false; }
-        };
-        template<class from> struct diff<double, from, false>
-        {
-            static dCONSTEXPR_CPP11 
-            bool can(const from) dNOEXCEPT { return false; }
-        };
-//.................................................................................
-        template<class ret, class from> struct help
-        {
-            enum { capacious = sizeof(ret) >= sizeof(from) };
-            typedef diff<ret, from, capacious> x;
-
-            static dCONSTEXPR_CPP11 
-            bool can(const from v) dNOEXCEPT { return x::can(v); }
-        };
-
-        template<class t> struct help<t, t>
-        {
-            static dCONSTEXPR_CPP11 
-            bool can(const t) dNOEXCEPT { return true; }
-        };
-
-    } // namespace detail_cast
-
-    template<class ret, class from>
-    dCONSTEXPR_CPP11 dNODISCARD 
-    bool can_numeric_cast(const from v) dNOEXCEPT
-    {
-        namespace x = ::tools::detail_cast;
-        typedef typename remove_cv<ret>::type 
-            ret_t;
-        typedef typename remove_cv<from>::type 
-            from_t;
-        typedef x::help<ret_t, from_t> 
-            agent;
-        return agent::can(v);
-    }
-
-} // tools
-#endif // !dTOOLS_NUMERIC_CAST_USED_
-#endif
-
-//================================================================================
-//================================================================================
-//================================================================================
-//================================================================================
-//================================================================================
-//================================================================================
-
-#if 0
-// [2020y-09-25d][20:13:18] Idrisov Denis R.
-// [2020y-12-01d][14:06:17] Idrisov Denis R.
-// [2021y-02-20d][18:40:18] Idrisov Denis R.
-#pragma once
-#ifndef dTOOLS_NUMERIC_CAST_USED_ 
-#define dTOOLS_NUMERIC_CAST_USED_ 1
-
-#include <tools/features.hpp>
-
-//#include <type_traits>
-//#include <stdexcept>
-//#include <typeinfo>
-//#include <cassert>
-//#include <string>
-//#include <limits>
-
-//================================================================================
-//================================================================================
-#ifndef dTOOLS_ENUM_TYPE_USED_ 
-#define dTOOLS_ENUM_TYPE_USED_ 1
-namespace tools
-{
-    namespace detail
-    {
-        template<class t, bool = ::std::is_enum<t>::value >
-        struct type_of_enum
-        {
-            using type = ::std::underlying_type_t<t>;
-        };
-
-        template<class t>
-        struct type_of_enum<t, false>
-        {
-            using type = t;
-        };
-
-    } // namespace detail
-
-    template<class t> 
-    using type_of_enum_t
-        = typename ::tools::detail::type_of_enum<t>::type;
-
-} // tools
-#endif // !dTOOLS_ENUM_TYPE_USED_
-
-//================================================================================
-//================================================================================
-namespace tools
-{
-    using str_t = ::std::string;
-
-    namespace detail_numeric_cast
-    {
-        #define dDETAIL \
-            ::tools::detail_numeric_cast
-
-        template<class ret, class from> ret throw_error(const from val)
-        {
-            using from_ = ::tools::type_of_enum_t<from>;
-            const from_ v = static_cast<from_>(val);
-            const str_t t = typeid(ret).name();
-            const str_t s = ::std::to_string(v);
-            throw ::std::out_of_range(
-                "tools::can_numeric_cast<" + t + ">(" + s + "): failed"
-            );
-        }
-
-        template<class from, class to, class t>
-        dCONSTEXPR_CPP11 from cast(const t value) dNOEXCEPT
-        {
-            using from_ = ::tools::type_of_enum_t<from>;
-            using to_   = ::tools::type_of_enum_t<to>;
-
-            using x = ::std::common_type_t<from_, to_>;
-            return static_cast<x>(value);
-        }
-
-        #define dMIN_LIMIT(to)                     \
-            dDETAIL::cast<from, to>(               \
-                (::std::numeric_limits<to>::min)() \
-            )
-
-        #define dMAX_LIMIT(to)                     \
-            dDETAIL::cast<from, to>(               \
-                (::std::numeric_limits<to>::max)() \
-            )
-
-        #define dTHE_VALUE(val) \
-            dDETAIL::cast<from, ret>(val)
-
-        template<class ret_type, class from_type>
-        class help
-        {
-            using origin_from 
-                = std::remove_cv_t<from_type>;
-
-            using origin_ret  
-                = std::remove_cv_t<ret_type>;
-
-            using from 
-                = ::tools::type_of_enum_t<origin_from>;
-
-            using ret  
-                = ::tools::type_of_enum_t<origin_ret>;
-        };
-
-    } // namespace detail_numeric_cast
-
-    #undef dTHE_VALUE
-    #undef dMIN_LIMIT
-    #undef dMAX_LIMIT
-    #undef dDETAIL
-    #undef dBOOL
-
-} // namespace tools
-
-//================================================================================
-//================================================================================
-
-namespace tools
-{
-    template<class ret, class from>
-    dCONSTEXPR_CPP11 ret assert_numeric_cast(const from val) dNOEXCEPT
-    {
-        assert((tools::can_numeric_cast<ret, from>(val)));
-        return static_cast<ret>(val);
-    }
-
-    template<class ret, class from>
-    dCONSTEXPR_CPP11 ret numeric_cast(const from val) 
-    {
-        return tools::can_numeric_cast<ret, from>(val) ?
-            static_cast<ret>(val):
-            detail_numeric_cast::throw_error<ret>(val);
-    }
-
-} // namespace tools
-//================================================================================
-
-#endif // !dTOOLS_NUMERIC_CAST_USED_
-#endif
-
